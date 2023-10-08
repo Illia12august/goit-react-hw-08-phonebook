f578fd91008d4b3f8904ca02d8731031ea6d591d
Illia12august <121337310+Illia12august@users.noreply.github.com>
2023-10-08 16:45:32 +0300

    copy from 7 hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import { ContactList } from './ContactList/ContactList';
+import { GlobalStyle, SectionContainer } from './GlobalStyle';
+import { QuizForm } from './QuizForm/QuizForm';
+
 export const App = () => {
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <SectionContainer>
+      <QuizForm />
+      <ContactList />
+      <GlobalStyle />
+    </SectionContainer>
   );
 };
